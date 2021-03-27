ec23eeecd3ce60ec4a35aa001fe13a9e60adf74e
Gabriel Lima <gabriellfsouza@gmail.com>
2021-03-27 18:26:05 +0000

    test debugging

--- src/app/utils/winston-utils/sample.spec.ts
+++ src/app/utils/winston-utils/sample.spec.ts
@@ -4,6 +4,6 @@ describe('winstonConfig', () => {
   it('should be able to validate a simple equation', () => {
     expect(logger).toHaveProperty('info');
     expect(logger).toHaveProperty('debug');
-    expect(logger).toHaveProperty('warn');
+    expect(logger).toHaveProperty('error');
   });
 });
